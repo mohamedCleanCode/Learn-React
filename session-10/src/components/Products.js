b3635fdf1376012defa509d69827aaa9a6627398
mohamed <m635379@gmail.com>
2023-01-16 01:42:18 +0200

    add rtk, products-slice and add products to page

--- session-10/src/components/Products.js
+++ session-10/src/components/Products.js
@@ -1,10 +1,40 @@
-import { Container } from "react-bootstrap";
+import { useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../rtk/slices/products-slice";
 
 function Products() {
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
   return (
     <div className="products">
       <Container>
         <h1>Hello Products</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product.id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  style={{ width: "200px", height: "200px", margin: "auto" }}
+                  variant="top"
+                  src={product.image}
+                />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>{product.description}</Card.Text>
+                  <Card.Text>{product.price}</Card.Text>
+                  <Button variant="primary">Add To Cart</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </div>
   );
--- session-10/src/index.js
+++ session-10/src/index.js
@@ -2,15 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { store } from "./rtk/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
